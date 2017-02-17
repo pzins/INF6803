11e632588976a72b1749ed2a5072ae5696b3a08a
Pierre <zins.pierre@gmail.com>
2017-02-17 14:04:04 -0500

    fix bugs

--- TP2/src/tp2/MyTracker.cpp
+++ TP2/src/tp2/MyTracker.cpp
@@ -22,10 +22,10 @@ std::vector<int> getHistogram(const cv::Mat& frame){
     int scale = 1;
     int delta = 0;
 
-    cv::GaussianBlur( frame, frame, cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT );
+//    cv::GaussianBlur( frame, frame, cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT );
 
     /// Convert it to gray
-    cvtColor( frame, frame, CV_BGR2GRAY );
+//    cvtColor( frame, frame, CV_BGR2GRAY );
 
     /// Generate grad_x and grad_y
     cv::Mat grad_x, grad_y;
@@ -40,8 +40,7 @@ std::vector<int> getHistogram(const cv::Mat& frame){
 
     /// Total Gradient (approximate)
     cv::addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, frame);
-
-    std::vector<int> histo(360);
+    std::vector<int> histo(360,0);
     for(int i = 0; i < frame.rows; ++i){
         for(int j = 0; j < frame.cols; ++j){
             float angle = cv::fastAtan2(grad_x.at<uint8_t>(i,j), grad_y.at<uchar>(i,j));
@@ -57,6 +56,14 @@ void MyTracker::initialize(const cv::Mat& oInitFrame, const cv::Rect& oInitBBox)
 }
 
 
+int getDistanceHistogram(const std::vector<int>& refHist, const std::vector<int>& currHist){
+    float somme = 0;
+    for(int i = 0; i < refHist.size(); ++i)
+    {
+        somme += sqrt(refHist.at(i) * currHist.at(i));
+    }
+    return -log(somme);
+}
 
 
 void MyTracker::apply(const cv::Mat &oCurrFrame, cv::Rect &oOutputBBox)
@@ -69,14 +76,28 @@ void MyTracker::apply(const cv::Mat &oCurrFrame, cv::Rect &oOutputBBox)
     for(int i = 0; i < 10; ++i){
         float x = myBox.x+round(2*myBox.width*dis(gen));
         float y = myBox.y+round(2*myBox.height*dis(gen));
-        int regionSizeW =  myBox.width + round(myBox.width/(3*dis(gen)));
-        int regionSizeH =  myBox.height+ round(myBox.height/(3*dis(gen)));
-        boxes.push_back(cv::Rect(x, y, regionSizeW, regionSizeH));
+        int regionSizeW =  myBox.width + round((myBox.width/3)*dis(gen));
+        int regionSizeH =  myBox.height+ round((myBox.height/3)*dis(gen));
+        boxes.push_back(cv::Rect(std::max(int(x),0), std::max(int(y),0), std::min(regionSizeW, oCurrFrame.rows), std::min(regionSizeH, oCurrFrame.cols)));
     }
-    for(auto i : boxes)
+    //get frame from ref rect + compute histo
+    cv::Mat myBoxFrame = oCurrFrame(myBox);
+    std::vector<int> ref_histo = getHistogram(myBoxFrame);
+    int mini_diff = 10000, mini_idx = -1;
+    for(int i = 0; i < boxes.size(); ++i)
     {
-        cv::rectangle(oCurrFrame, cv::Point(i.x, i.y), cv::Point(i.x+i.size().width, i.y+i.size().height), cv::Scalar(255,0,0));
+        cv::rectangle(oCurrFrame, cv::Point(boxes.at(i).x, boxes.at(i).y), cv::Point(boxes.at(i).x+boxes.at(i).size().width, boxes.at(i).y+boxes.at(i).size().height), cv::Scalar(255,0,0));
+//        std::cout << boxes.at(i).x << " "  << boxes.at(i).y << " " << boxes.at(i).height << " "  << boxes.at(i).y+boxes.at(i).width << std::endl;
+        std::vector<int> curr_histo = getHistogram(oCurrFrame(boxes.at(i)));
+        if(abs(getDistanceHistogram(ref_histo, curr_histo)) <= mini_diff)
+        {
+            mini_idx = i;
+        }
+        std::cout << "ol" << std::endl;
     }
+    myBox = boxes.at(mini_idx);
     boxes.clear();
 
 }
+
+//voir function normalize()
