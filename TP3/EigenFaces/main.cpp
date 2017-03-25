6f53cde3e8984a3f971c5a860144efed70795c71
Pierre <zins.pierre@gmail.com>
2017-03-25 17:45:28 -0400

    clean version

--- TP3/EigenFaces/main.cpp
+++ TP3/EigenFaces/main.cpp
@@ -8,10 +8,12 @@
 #include <set>
 
 
-using	namespace	std;
-using	namespace	cv;
-
-
+#define NB_PERSONS 40
+#define NB_IMG_PER_PERSON 10
+#define DATASET NB_IMG_PER_PERSON * NB_PERSONS
+#define IMG_W 92
+#define IMG_H 112
+#define K 10
 struct MyEigen {
     double value;
     int index;
@@ -37,133 +39,114 @@ double dist(const std::vector<double>& currImgWi, const std::vector<double>& dat
 int main()
 {
     cv::Mat avgImg;
-    avgImg.create(112, 92,CV_32FC1);
-    std::map<int, string> mapData;
-    for(int i = 1; i <= 40; ++i)
+    avgImg.create(IMG_H, IMG_W, CV_64F);
+    std::map<int, std::string> mapData;
+    std::map<int, cv::Mat> faces;
+    for(int i = 1; i <= NB_PERSONS; ++i)
     {
-        for(int j = 1; j <= 10; ++j)
+        for(int j = 1; j <= NB_IMG_PER_PERSON; ++j)
         {
             std::stringstream ss;
-            string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
+            std::string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
             ss << path << "s" << i << "_" << j << ".pgm";
-            mapData.insert(std::pair<int,string>((i-1)*10+j-1, ss.str()));
-
-            cv::Mat img = cv::imread(ss.str(), IMREAD_GRAYSCALE);
+            cv::Mat img = cv::imread(ss.str(), cv::IMREAD_GRAYSCALE);
+            mapData.insert(std::pair<int,std::string>((i-1)*10+j-1, ss.str()));
+            faces.insert(std::pair<int, cv::Mat>((i-1)*10+j-1, img));
             cv::accumulate(img, avgImg);
         }
     }
-    avgImg /= 400;
-    avgImg.convertTo(avgImg, CV_8UC1);
+    avgImg /= DATASET;
+    avgImg.convertTo(avgImg, CV_8UC1); //à voir si grave
 
-    cv::Mat matrix(112*92, 400, CV_32FC1);
-    for(int i = 1; i <= 40; ++i)
+    cv::Mat matrix(IMG_H*IMG_W, DATASET, CV_64F);
+    for(int i = 0; i < faces.size(); ++i)
     {
-        for(int j = 1; j <= 10; ++j)
-        {
-            std::stringstream ss;
-            string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
-            ss << path << "s" << i << "_" << j << ".pgm";
-            cv::Mat img = cv::imread(ss.str(), IMREAD_GRAYSCALE);
+        cv::subtract(faces.at(i), avgImg, faces.at(i));
+        faces.at(i) = faces.at(i).reshape(0,IMG_H*IMG_W);
+        faces.at(i).col(0).copyTo(matrix.col(i));//(i-1)*10+j-1));
 
-            cv::subtract(img, avgImg, img);
-            img = img.reshape(0,112*92);
-            img.col(0).copyTo(matrix.col((i-1)*10+j-1));
-        }
     }
-    std::cout << matrix.rows << " " << matrix.cols << std::endl;
-    matrix.convertTo(matrix, CV_64F);
 
+    //transpose matrix
     cv::Mat matrixT;
     cv::transpose(matrix, matrixT);
 
-    Mat cov, mu;
+    //compute covariance matrix
+    //matrixT * matrix instead of matrix * matrixT : to limit the dimensionnality
+    cv::Mat cov, mu;
     cov = matrixT * matrix;
-    std::cout << cov.size() << std::endl;
 //    cv::calcCovarMatrix(matrixT, cov, mu, CV_COVAR_NORMAL | CV_COVAR_COLS);
 
-//    cov = cov / (matrix.rows - 1);
-    std::cout << "ol" << std::endl;
+    //compute eigenvalues and eigenvectors
     cv::Mat eVa, eVe;
     cv::eigen(cov, eVa, eVe);
 //    std::cout << eVe << std::endl;
 //    std::cout << eVa << std::endl;
-    std::cout << eVa.rows << " " << eVa.cols << std::endl;
-    std::cout << eVe.rows << " " << eVe.cols << " " << eVe.type() << std::endl;
-    std::cout << eVa.at<double>(0,0) << std::endl;
-    double v = eVa.at<double>(0,0) ;
-    cv::Mat res(112*92,400, CV_64F);
-    std::set<MyEigen> mySet;
-    for(int i = 0; i < eVe.cols; ++i){
-        res.col(i) = matrix * eVe.col(i);
-        cv::normalize(res.col(i),res.col(i));
-        mySet.insert(MyEigen(eVa.at<double>(i,0),i));
-    }
-    double s = 0;
-    for(int i = 0; i < res.rows;++i)
-        s += res.at<double>(i,2);
-    std::cout << "##  " << s << std::endl;
-    int count = 0;
-    string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
 
-    cv::Mat image = cv::imread( "/home/pierre/Dev/INF6803/TP3/DATA/l.png", IMREAD_GRAYSCALE);
-    std::cout << image.size() << std::endl;
+    //real Eigenvector of matrix * matrixT
+    cv::Mat realEve(IMG_H*IMG_W, K, CV_64F);
+    std::set<MyEigen> mySet;
+    for(int i = 0; i < K; ++i){
+        realEve.col(i) = matrix * eVe.col(i);
+        cv::normalize(realEve.col(i),realEve.col(i));
+//        mySet.insert(MyEigen(eVa.at<double>(i,0),i));
+    }
+    /**
+    double s = 0;
+    for(int i = 0; i < realEve.rows;++i)
+        s += realEve.at<double>(i,2);
+    std::cout << "##  " << s << std::endl;
+    **/
+    int count = 0;
+    std::string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
+
+    //load new image
+    cv::Mat image = cv::imread( "/home/pierre/Dev/INF6803/TP3/DATA/s2_2.pgm", cv::IMREAD_GRAYSCALE);
     cv::subtract(image, avgImg, image);
-    image = image.reshape(0,112*92);
+    image = image.reshape(0,IMG_H*IMG_W);
     image.convertTo(image, CV_64F);
 
-    cv::Mat phi_hat;
 
-    std::vector<double> phi;
-    phi_hat.create(112 * 92,1 ,CV_64F);
+    cv::Mat phi_hat(IMG_H*IMG_W, 1, CV_64F);
+    std::vector<double> omega;
 
-    for(std::set<MyEigen>::iterator i = mySet.begin(); i != mySet.end(); i++) {
-        if(count++ == 9) break;
-        cv::Mat tmp = res.col((*i).index);
+    for(int i = 0; i < K; ++i)
+    {
+        cv::Mat tmp = realEve.col(i);
         cv::Mat tmpT, wi;
         tmp.convertTo(tmp, CV_64F);
         cv::transpose(tmp,tmpT);
         wi = tmpT * image;
-        phi.push_back(wi.at<double>(0,0));
+        omega.push_back(wi.at<double>(0,0));
         cv::Mat res_ite;
         double wi_fact = wi.at<double>(0,0);
         res_ite = wi_fact * tmp;
         cv::accumulate(res_ite, phi_hat);
-
-
     }
+
     std::vector<std::vector<double>> dataset_wi;
-    for(int i = 1; i <= 40; ++i)
+    for(int i = 0; i < faces.size(); ++i)
     {
-        for(int j = 1; j <= 10; ++j)
+        std::vector<double> tmp;
+        for(int j = 0; j < K; ++j)
         {
-            std::stringstream ss;
-            string path = "/home/pierre/Dev/INF6803/TP3/DATA/";
-            ss << path << "s" << i << "_" << j << ".pgm";
-            cv::Mat img = cv::imread(ss.str(), IMREAD_GRAYSCALE);
-            cv::subtract(img, avgImg, img);
-            img = img.reshape(0,112*92);
-            img.convertTo(img, CV_64F);
-            std::vector<double> tmp;
-            int counter = 0;
-            for(std::set<MyEigen>::iterator i = mySet.begin(); i != mySet.end(); i++) {
-                if(counter++ == 9) break;
-                cv::Mat ui = res.col((*i).index);
-                cv::transpose(ui,ui);
-                ui.convertTo(ui, CV_64F);
-                cv::Mat m =  ui*img;
-                double res = m.at<double>(0,0);
-                tmp.push_back(res);
-            }
-            dataset_wi.push_back(tmp);
+            cv::Mat ui = realEve.col(j);
+            cv::transpose(ui,ui);
+            ui.convertTo(ui, CV_64F);
+            faces.at(i).convertTo(faces.at(i), CV_64F);
+            cv::Mat m =  ui*faces.at(i);
+            tmp.push_back(m.at<double>(0,0));
         }
+        dataset_wi.push_back(tmp);
     }
-    for(auto i : phi)
+
+    for(auto i : omega)
         std::cout << i << std::endl;
 
     double best_dist = 1000000000, best_idx = -1;
     for(int i = 0; i < dataset_wi.size(); ++i)
     {
-        double res = dist(phi, dataset_wi.at(i));
+        double res = dist(omega, dataset_wi.at(i));
         std::cout << res << std::endl;
         if(res < best_dist){
             best_dist = res;
@@ -178,6 +161,6 @@ int main()
 
     imshow("OL", avgImg);
     cv::waitKey(0);
-    namedWindow("OL");
+    cv::namedWindow("OL");
     return 0;
 }
