c28858976ab7b2563445b9d73bb49466b4124716
Pierre <zins.pierre@gmail.com>
2017-03-26 00:19:10 -0400

    save

--- TP3/EigenFaces/main.cpp
+++ TP3/EigenFaces/main.cpp
@@ -5,47 +5,30 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <set>
-
+#include <limits>
 
 #define NB_PERSONS 40
 #define NB_IMG_PER_PERSON 9
 #define DATASET NB_IMG_PER_PERSON * NB_PERSONS
 #define IMG_W 92
 #define IMG_H 112
-#define K 300
+#define K 10
 #define DISTANCE_THRESHOLD 10000
-#define DISTANCE 1 //0 = Mahalanobis, 1 = Euclidian
+#define SHOW_EIGENFACE false //No to activate if K is high
 
 void fillMat(const std::vector<double>& a, const std::vector<double>& b, cv::Mat& mat){
     for(int i = 0; i < a.size(); ++i)
     {
-        mat.at<double>(0,i) = a.at(i);
-        mat.at<double>(1,i) = b.at(i);
+        mat.at<double>(i,0) = a.at(i);
+        mat.at<double>(i,1) = b.at(i);
     }
 }
 
 double dist(const std::vector<double>& currImgWi, const std::vector<double>& datasetImgWi){
-    if(DISTANCE == 0)
-    {
-        cv::Mat mat(currImgWi.size(), 2, CV_64F);
-        fillMat(currImgWi, datasetImgWi, mat);
-        cv::Mat matT;
-        cv::transpose(mat, matT);
-        cv::Mat cov = mat * matT;
-        std::cout << cov<< std::endl;
-
-        cv::Mat res;
-        cv::invert(cov, res, cv::DECOMP_LU);
-        return cv::Mahalanobis(mat.col(0), mat.col(1), res);
-    }
-    else
-    {
-        double res = 0;
-        for(int i = 0; i < currImgWi.size(); ++i)
-            res += pow(datasetImgWi.at(i) - currImgWi.at(i),2);
-        return res;
-    }
+    double res = 0;
+    for(int i = 0; i < currImgWi.size(); ++i)
+        res += sqrt(pow(datasetImgWi.at(i) - currImgWi.at(i),2));
+    return res;
 }
 
 int main()
@@ -58,19 +41,30 @@ int main()
     {
         for(int j = 1; j <= NB_IMG_PER_PERSON; ++j)
         {
-            std::stringstream ss;
+            std::stringstream ss, s;
             std::string path = "/home/pierre/Dev/INF6803/TP3/DATA/training/";
             ss << path << "s" << i << "_" << j << ".pgm";
             cv::Mat img = cv::imread(ss.str(), cv::IMREAD_GRAYSCALE);
             int index = (i-1) * NB_IMG_PER_PERSON + j - 1;
-            mapData.insert(std::pair<int,std::string>(index, ss.str()));
+
+            //save images of faces
             faces.insert(std::pair<int, cv::Mat>(index, img));
+
+            //sum all faces to compute the mean
             cv::accumulate(img, avgImg);
+
+
+            //just for diplay purpose
+            s << "s" << i << "_" << j << ".pgm";
+            mapData.insert(std::pair<int,std::string>(index, s.str()));
         }
     }
+    //compue the mean
     avgImg /= DATASET;
-    avgImg.convertTo(avgImg, CV_8UC1); //à voir si grave
+    //convert because faces.at(i) is of type CV_8UC1 and subtract only work with same type
+    avgImg.convertTo(avgImg, CV_8UC1);
 
+    //compute mean for each face images + reshape + store in a matrix
     cv::Mat matrix(IMG_H*IMG_W, DATASET, CV_64F);
     for(int i = 0; i < faces.size(); ++i)
     {
@@ -88,34 +82,50 @@ int main()
     //matrixT * matrix instead of matrix * matrixT : to limit the dimensionnality
     cv::Mat cov, mu;
     cov = matrixT * matrix;
-//    cv::calcCovarMatrix(matrixT, cov, mu, CV_COVAR_NORMAL | CV_COVAR_COLS);
 
-    //compute eigenvalues and eigenvectors
+    //compute eigenvalues and eigenvectors of matrixT * matrix
     cv::Mat eVa, eVe;
     cv::eigen(cov, eVa, eVe);
 
 
-
-    //real Eigenvector of matrix * matrixT
+    //compute real Eigenvector of matrix * matrixT
     cv::Mat realEve(IMG_H*IMG_W, K, CV_64F);
     for(int i = 0; i < K; ++i){
         realEve.col(i) = matrix * eVe.col(i);
+        //normalization
         cv::normalize(realEve.col(i),realEve.col(i),1);
+
+        //print Eigenfaces
+        if(SHOW_EIGENFACE) {
+            cv::Mat tmp(realEve.rows,1,CV_64F);
+            realEve.col(i).copyTo(tmp);
+            tmp = tmp.reshape(1,IMG_H);
+            //normalize between 0 and 1
+            double mi, ma;
+            cv::minMaxLoc(tmp, &mi, &ma);
+            tmp += fabs(mi);
+            tmp /= fabs(ma)+fabs(mi);
+            //display
+            std::stringstream ss;
+            ss << "EigenVector_" << i;
+            cv::namedWindow(ss.str());
+            cv::imshow(ss.str(), tmp);
+        }
     }
 
+
     //debug eigenvectors
-    /**
+    /*
     double summ = 0;
     for(int i = 0; i < IMG_H*IMG_W; ++i)
     {
-        summ += pow(realEve.at<double>(0,i),2);
-        std::cout << realEve.at<double>(0,i) << " ";
+        summ += pow(realEve.at<double>(i,0),2);
     }
     std::cout << std::endl << sqrt(summ) << std::endl;
     return 0;
-    **/
+    */
 
-    //compute wi for all t1raining set images
+    //compute wi for all training set images
     std::vector<std::vector<double>> dataset_wi;
     for(int i = 0; i < faces.size(); ++i)
     {
@@ -132,6 +142,7 @@ int main()
         dataset_wi.push_back(tmp);
     }
 
+    /*
     for(int i = 0; i < dataset_wi.size(); ++i)
     {
         for(int j = 0; j < dataset_wi.at(i).size(); ++j)
@@ -140,9 +151,10 @@ int main()
         }
         std::cout << std::endl;
     }
+    */
 
     //load new image
-    cv::Mat image = cv::imread( "/home/pierre/Dev/INF6803/TP3/DATA/test/s26_10.pgm", cv::IMREAD_GRAYSCALE);
+    cv::Mat image = cv::imread( "/home/pierre/Dev/INF6803/TP3/DATA/test/s25_10.pgm", cv::IMREAD_GRAYSCALE);
     cv::subtract(image, avgImg, image);
     image = image.reshape(0,IMG_H*IMG_W);
     image.convertTo(image, CV_64F);
@@ -165,11 +177,15 @@ int main()
         cv::accumulate(res_ite, phi_hat);
     }
 
+    /*
     for(int i = 0;  i < omega.size(); ++i)
         std::cout << omega.at(i) << " ";
     std::cout << std::endl;
-    //check if the face is known
-    double best_dist = 1000000000, best_idx = -1;
+    */
+
+    //check if the face is known and who it is
+    std::cout << "Distance pour chaque visage : " << std::endl;
+    double best_dist = std::numeric_limits<int>::max(), best_idx = -1;
     for(int i = 0; i < dataset_wi.size(); ++i)
     {
         double res = dist(omega, dataset_wi.at(i));
@@ -179,11 +195,10 @@ int main()
             best_idx = i;
         }
     }
+    if(best_dist < DISTANCE_THRESHOLD)
+        std::cout << "(" << best_dist << ", " << best_idx << ")" << " => " << mapData.at(best_idx) << std::endl;
+    else
+        std::cout << "Ce visage est inconnu" << std::endl;
 
-    std::cout << "(" << best_dist << ", " << best_idx << ")" << " => " << mapData.at(best_idx) << std::endl;
-
-//    imshow("OL", avgImg);
-    cv::waitKey(0);
-    cv::namedWindow("OL");
     return 0;
 }
